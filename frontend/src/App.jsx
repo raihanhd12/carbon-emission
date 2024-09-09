466cde328f5a734af8ba7ef547594f0f04ee92d6
DaffaPoseidon <ndap.cahyo@gmail.com>
2024-09-09 14:05:05 +0700

    Connect Wallet (Selesai) + Landing Page 1.0

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
-import './App.scss'
-import ConnectWallet from './ConnectWallet'
+import './LandingPage/App.scss'
+import ConnectWallet from './ConnectWallet/ConnectWallet'
+import LandingPage from './LandingPage/LandingPage'
 import Login from "./Login"
 
 function App () {
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./App.scss";
+// import "./LandingPage/App.scss";
+import "./ConnectWallet/App.scss"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
